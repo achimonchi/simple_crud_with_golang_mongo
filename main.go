83cd0fdc4ef7ede381703b764036ec61362e39a9
Reyhan Jovie <achimonchi.reyhan@gmail.com>
2020-04-04 11:45:28 +0700

    set connection

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/achimonchi/belajar_crud_mongodb/config"
+)
 
 func main() {
 	fmt.Println("CRUD with Go and Mongo")
+
+	_, err := config.GetMongoDB()
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
